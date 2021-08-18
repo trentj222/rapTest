dacefcc0e2cb60a75927d3e27355df728f6cb3d8
Trent Jesukaitis <tjesukaitis@autopay.com>
2021-08-18 08:30:41 -0600

    Saucelabs testing

--- wdio.conf.js
+++ wdio.conf.js
@@ -7,6 +7,11 @@ exports.config = {
     // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
     // on a remote machine).
     runner: 'local',
+    user: process.env.SAUCE_USERNAME,
+    key: process.env.SAUCE_ACCESS_KEY,
+    region: 'us',
+    services: ['sauce'],
+
     //
     // ==================
     // Specify Test Files
@@ -46,25 +51,19 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 1,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
     capabilities: [{
-    
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
-        maxInstances: 1,
-        //
-        browserName: 'chrome',
-        acceptInsecureCerts: true
-        // If outputDir is provided WebdriverIO can capture driver session logs
-        // it is possible to configure which logTypes to include/exclude.
-        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-        // excludeDriverLogs: ['bugreport', 'server'],
+        maxInstances: 5,
+        browserName: 'firefox',
+        browserVersion: 'latest',
+        platformName: 'Windows 10',
+        'sauce:options': {
+            build: `Build ${Math.ceil(Date.now() / 1000)}`
+        },
     }],
     //
     // ===================
@@ -113,8 +112,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
-    
+
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: https://webdriver.io/docs/frameworks
@@ -138,7 +136,7 @@ exports.config = {
     reporters: [['allure', {outputDir: 'allure-results'}]],
 
 
-    
+
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
